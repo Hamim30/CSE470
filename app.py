a6c0f0ce958063550930be835f53f2dc0ade40e1
fatehaPrintia <fatehaprintia@gmail.com>
2023-10-23 14:12:01 +0600

    Hello 470

--- app.py
+++ app.py
@@ -17,21 +17,38 @@ class Resources(db.Model):
     Course_Code = db.Column(db.String(10), nullable=False)
     f_type = db.Column(db.String(10), nullable=False)
     Description = db.Column(db.Text, nullable=False)
-
-
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self) -> str:
-        return f"{self.Course_Code} - {self.Description}"
+    # def __repr__(self) -> str:
+    #     return f"{self.Course_Code} - {self.Description}"
     
 @app.route('/')
 def home():
+    resource=Resources(email='admin.gmail',student_id='20301357',Course_Code='cse110',f_type='lab',Description='lab files')
+    db.session.add(resource)
+    db.session.commit()
+    
+    
     return render_template('index.html')
 
 
-@app.route('/Add_resources')
+@app.route('/Add_resources',methods=['GET','POST'])
 def Add_resources():
+    if request.method=="POST":
+        print(request.form['Course_Code'])
     return render_template('Add_resources.html')
+
+@app.route('/view')
+def View():
+    all_resource=Resources.query.all()
+    return render_template('view.html',all_resource=all_resource)
+
+
+@app.route('/show')
+def Show():
+    all_resource=Resources.query.all()
+    print(all_resource)
+    return 'show'
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
