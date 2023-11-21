e5dffb35bceec21a3e26fc9325b1b227c96ee1e1
danialcodes <danialcodes@gmail.com>
2023-11-21 19:30:29 +0600

    [Modified: Duplicate code and css]

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-from flask import Flask, render_template, request, Response,send_file,send_from_directory,redirect, url_for
+from flask import (
+    Flask,
+    render_template,
+    request,
+    Response,
+    send_file,
+    send_from_directory,
+    redirect,
+    url_for,
+)
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
 from werkzeug.utils import secure_filename
 from itertools import groupby
-from model import Resources,app,db,Comment,Faculty
+from model import Resources, app, db, Comment, Faculty
 
 
-
-
-
-@app.route('/postt')
-def postt():
+@app.route("/thesis")
+def thesis():
     comments = Comment.query.filter_by(parent_id=None).all()
-    return render_template('postt.html', comments=comments)
+    return render_template("thesis.html", comments=comments)
 
-@app.route('/add_comment', methods=['POST'])
+
+@app.route("/add_comment", methods=["POST"])
 def add_comment():
-    text = request.form.get('comment_text')
-    parent_id = request.form.get('parent_id', None)
+    text = request.form.get("comment_text")
+    parent_id = request.form.get("parent_id", None)
 
     new_comment = Comment(text=text, parent_id=parent_id)
     db.session.add(new_comment)
     db.session.commit()
 
-    return redirect(url_for('postt'))
-@app.route('/delete_comment/<int:comment_id>', methods=['GET'])
+    return redirect(url_for("thesis"))
+
+
+@app.route("/delete_comment/<int:comment_id>", methods=["GET"])
 def delete_comment(comment_id):
     comment_to_delete = Comment.query.get(comment_id)
 
@@ -34,36 +43,40 @@ def delete_comment(comment_id):
         db.session.delete(comment_to_delete)
         db.session.commit()
 
-    return redirect(url_for('postt'))
+    return redirect(url_for("thesis"))
 
-@app.route('/')
+
+@app.route("/")
 def homme():
-    return render_template('index.html')
-@app.route('/index')
+    return render_template("index.html")
+
+
+@app.route("/index")
 def home():
-    return render_template('index.html')
-@app.route('/courseinfo')
+    return render_template("index.html")
+
+
+@app.route("/courseinfo")
 def cinfo():
-    return render_template('courseinfo.html')
+    return render_template("courseinfo.html")
 
 
-@app.route('/faculty_form', methods=['GET', 'POST'])
+@app.route("/faculty_form", methods=["GET", "POST"])
 def faculty_form():
-    if request.method == 'POST':
-        full_name = request.form['full_name']
-        initial = request.form['initial']
-        email = request.form['email']
-        thesis_supervision = request.form['thesis_supervision']
-        research_interest = request.form['research_interest']
-        #routine = request.files['routine'].read()  # Read image file as binary data
-        routine_file = request.files['routine']
+    if request.method == "POST":
+        full_name = request.form["full_name"]
+        initial = request.form["initial"]
+        email = request.form["email"]
+        thesis_supervision = request.form["thesis_supervision"]
+        research_interest = request.form["research_interest"]
+        # routine = request.files['routine'].read()  # Read image file as binary data
+        routine_file = request.files["routine"]
 
-        #routine = request.files['routine']
-        #filename = secure_filename(routine.filename) 
+        # routine = request.files['routine']
+        # filename = secure_filename(routine.filename)
 
-        
         filename = secure_filename(routine_file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         routine_file.save(file_path)
 
         new_faculty = Faculty(
@@ -72,140 +85,151 @@ def faculty_form():
             email=email,
             thesis_supervision=thesis_supervision,
             research_interest=research_interest,
-            routine=filename  # Store the file name in the database
-        )   
-       
-      
+            routine=filename,  # Store the file name in the database
+        )
 
-        
-        
         db.session.add(new_faculty)
         db.session.commit()
 
-    
-    return render_template('faculty_form.html')
+    return render_template("faculty_form.html")
 
 
-@app.route('/faculty')
+@app.route("/faculty")
 def faculty():
     faculty = Faculty.query.all()
-    return render_template('faculty.html', faculty=faculty)
+    return render_template("faculty.html", faculty=faculty)
 
-@app.route('/faculty_description/<int:sno>')
+
+@app.route("/faculty_description/<int:sno>")
 def delete(sno):
     faculty = Faculty.query.filter_by(sno=sno).first()
-    
-    
-    return render_template('faculty_description.html', faculty=faculty)
+
+    return render_template("faculty_description.html", faculty=faculty)
 
 
-
-@app.route('/Add_resources', methods=['GET', 'POST'])
-def Add_resources():
+@app.route("/add_resources", methods=["GET", "POST"])
+def add_resources():
     filename2 = None
     if request.method == "POST":
-        Course_Code = request.form['Course_Code']
-        email = request.form['email']
-        student_id = request.form['student_id']
-        f_type = request.form['f_type']
-        Description = request.form['Description']
+        Course_Code = request.form["Course_Code"]
+        email = request.form["email"]
+        student_id = request.form["student_id"]
+        f_type = request.form["f_type"]
+        Description = request.form["Description"]
 
-
-        up_file = request.files['up_file']
+        up_file = request.files["up_file"]
 
         if up_file:
-            
             # file_data = up_file.read()
             filename2 = up_file.filename  # Get the filename
-           
+
             resource = Resources(
-                email=email, up_file=filename2, student_id=student_id, Course_Code=Course_Code,
-                f_type=f_type, Description=Description
+                email=email,
+                up_file=filename2,
+                student_id=student_id,
+                Course_Code=Course_Code,
+                f_type=f_type,
+                Description=Description,
             )
 
             db.session.add(resource)
             db.session.commit()
-            up_file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(up_file.filename)))
+            up_file.save(
+                os.path.join(
+                    app.config["UPLOAD_FOLDER"], secure_filename(up_file.filename)
+                )
+            )
     all_resource = Resources.query.all()
-    return render_template('Add_resources.html', all_resource=all_resource, filename2=filename2)
+    return render_template(
+        "add_resources.html", all_resource=all_resource, filename2=filename2
+    )
 
 
-@app.route('/view')
+@app.route("/view")
 def View():
-    all_resource=Resources.query.all()
-    return render_template('view.html',all_resource=all_resource)
-@app.route('/community')
+    all_resource = Resources.query.all()
+    return render_template("view.html", all_resource=all_resource)
+
+
+@app.route("/community")
 def community():
-    return render_template('community.html')
+    return render_template("community.html")
 
 
-@app.route('/Alumni')
+@app.route("/Alumni")
 def Alumni():
-    return render_template('Alumni.html')
-@app.route('/get_file/<int:sno>')
+    return render_template("Alumni.html")
+
+
+@app.route("/get_file/<int:sno>")
 def get_file(sno):
     resource = Resources.query.get_or_404(sno)
-    return Response(resource.up_file, content_type="application/octet-stream")  # Update the content type based on the file type
+    return Response(
+        resource.up_file, content_type="application/octet-stream"
+    )  # Update the content type based on the file type
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return "No file part"
 
-    file = request.files['file']
+    file = request.files["file"]
 
-    if file.filename == '':
+    if file.filename == "":
         return "No selected file"
 
     # Access the file name
     filename = file.filename
 
     # You can now use the 'filename' variable as needed
-    return f'Uploaded file name: {filename}'
-@app.route('/download/<filename>')
+    return f"Uploaded file name: {filename}"
+
+
+@app.route("/download/<filename>")
 def download(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    return send_from_directory(
+        app.config["UPLOAD_FOLDER"], filename, as_attachment=True
+    )
 
-@app.route('/serve_file/<filename>')
+
+@app.route("/serve_file/<filename>")
 def serve_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
-
-
-
-
-@app.route('/cse1')
+@app.route("/cse1")
 def cse1():
-    all_resource=Resources.query.filter(Resources.Course_Code.like('cse1%')).all()
+    all_resource = Resources.query.filter(Resources.Course_Code.like("cse1%")).all()
     grouped_resources = {}
-    
+
     # Group resources by course code
     all_resource.sort(key=lambda x: x.Course_Code)
     for course_code, resources in groupby(all_resource, key=lambda x: x.Course_Code):
         grouped_resources[course_code] = list(resources)
-    
-    return render_template('cse1.html', grouped_resources=grouped_resources)
+
+    return render_template("cse1.html", grouped_resources=grouped_resources)
+
 
 comments = []
 ratings = []
-Serials=[]
-course_codes=[]
+Serials = []
+course_codes = []
 
 
-@app.route('/comment')
+@app.route("/comment")
 def comment():
-    return render_template('comment.html', course_data=zip(course_codes, Serials, comments, ratings))
+    return render_template(
+        "comment.html", course_data=zip(course_codes, Serials, comments, ratings)
+    )
 
 
-
-@app.route('/submit', methods=['POST'])
+@app.route("/submit", methods=["POST"])
 def submit():
-    comment = request.form.get('comment')
-    rating = request.form.get('rating')
-    course_code=request.form.get('course_code')
-    Serial=request.form.get('num')
-   
+    comment = request.form.get("comment")
+    rating = request.form.get("rating")
+    course_code = request.form.get("course_code")
+    Serial = request.form.get("num")
 
     if comment and rating and course_code and Serial:
         comments.append(comment)
@@ -213,95 +237,99 @@ def submit():
         course_codes.append(course_code)
         Serials.append(Serial)
 
-    return redirect(url_for('comment'))
+    return redirect(url_for("comment"))
 
 
-#alumny
-names= []
+# alumny
+names = []
 emails = []
-c_names=[]
-dess=[]
-c_types=[]
+c_names = []
+dess = []
+c_types = []
 
-@app.route('/alumni')
+
+@app.route("/alumni")
 def alumni():
-   
-    return render_template('alumni.html', alumnyy=zip(names, emails, c_names,c_types, dess))
+    return render_template(
+        "alumni.html", alumnyy=zip(names, emails, c_names, c_types, dess)
+    )
 
-@app.route('/submitt', methods=['POST'])
+
+@app.route("/submitt", methods=["POST"])
 def submitt():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    cname=request.form.get('cname')
-    c_type=request.form.get('c_type')
-    info=request.form.get('info')
-   
+    name = request.form.get("name")
+    email = request.form.get("email")
+    cname = request.form.get("cname")
+    c_type = request.form.get("c_type")
+    info = request.form.get("info")
+
     if name and email and cname and c_type and info:
         names.append(name)
         emails.append(email)
         c_names.append(cname)
         c_types.append(c_type)
         dess.append(info)
-  
 
-    return redirect(url_for('alumni'))
+    return redirect(url_for("alumni"))
 
-#Event
-e_names= []
+
+# Event
+e_names = []
 e_venues = []
-e_des=[]
-e_infos=[]
+e_des = []
+e_infos = []
 
 
-@app.route('/event')
+@app.route("/event")
 def event():
-   
-    return render_template('event.html', eventt=zip(e_names,e_venues, e_des, e_infos))
+    return render_template("event.html", eventt=zip(e_names, e_venues, e_des, e_infos))
 
-@app.route('/submittt', methods=['POST'])
+
+@app.route("/submittt", methods=["POST"])
 def submittt():
-    name = request.form.get('name')
-    venu = request.form.get('venu')
-    edes=request.form.get('edes')
-    info=request.form.get('info')
-   
+    name = request.form.get("name")
+    venu = request.form.get("venu")
+    edes = request.form.get("edes")
+    info = request.form.get("info")
+
     if name and venu and edes and info:
         e_names.append(name)
         e_venues.append(venu)
         e_des.append(edes)
         e_infos.append(info)
-  
 
-    return redirect(url_for('event'))
+    return redirect(url_for("event"))
 
 
-#Internship
-i_names= []
+# Internship
+i_names = []
 positions = []
-requiremments=[]
-i_infos=[]
+requiremments = []
+i_infos = []
 
 
-@app.route('/intern')
+@app.route("/intern")
 def intern():
-   
-    return render_template('intern.html', internn=zip(i_names,positions, requiremments, i_infos))
+    return render_template(
+        "intern.html", internn=zip(i_names, positions, requiremments, i_infos)
+    )
 
-@app.route('/submitttt', methods=['POST'])
+
+@app.route("/submitttt", methods=["POST"])
 def submitttt():
-    name = request.form.get('name')
-    pos = request.form.get('pos')
-    req=request.form.get('req')
-    info=request.form.get('info')
-   
+    name = request.form.get("name")
+    pos = request.form.get("pos")
+    req = request.form.get("req")
+    info = request.form.get("info")
+
     if name and pos and req and info:
         i_names.append(name)
         positions.append(pos)
         requiremments.append(req)
         i_infos.append(info)
-  
 
-    return redirect(url_for('intern'))
+    return redirect(url_for("intern"))
+
 
 if __name__ == "__main__":
     with app.app_context():
--- model.py
+++ model.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///resources.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///resources.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["UPLOAD_FOLDER"] = "uploads"
 db = SQLAlchemy(app)  # Use a single SQLAlchemy instance for the app
+
 
 class Resources(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
@@ -18,11 +19,21 @@ class Resources(db.Model):
     up_file = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500))
-    parent_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
-    replies = db.relationship('Comment', backref=db.backref('parent', remote_side=[id]))
+    parent_id = db.Column(db.Integer, db.ForeignKey("comment.id"))
+    replies = db.relationship("Comment", backref=db.backref("parent", remote_side=[id]))
+
+class Thesis(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.String(128))
+    topic = db.Column(db.String(128))
+    isApproved = db.Column(db.Boolean)
+
+
+
 
 class Faculty(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
@@ -35,11 +46,9 @@ class Faculty(db.Model):
 
     def __repr__(self) -> str:
         return f"{self.sno}, Name: {self.full_name}, Email: {self.email}, Interest in Supervising: {self.thesis_supervision}, Research Interest: {self.research_interest}"
-    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
-
         db.create_all()
     app.run(debug=True)
